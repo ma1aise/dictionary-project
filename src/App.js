82d5cdb557b16882079549d9a87775f9f1e7eb7b
ma1aise <lisaewells03@outlook.com>
2024-03-18 16:39:36 +1000

    Created Dictionary component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src="logo.png" className="App-logo img-fluid" alt="logo" />
-      </header>
-      Hello
+      <div className="container">
+        <header className="App-header">
+          <img src="logo.png" className="App-logo img-fluid" alt="logo" />
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">
+          This project was coded by Lisa Wells and is{" "}
+          <a
+            href="https://github.com/ma1aise/dictionary-project"
+            target="blank"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
