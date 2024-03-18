6672f4d290ca938358c6927cf79839a991c55524
ma1aise <lisaewells03@outlook.com>
2024-03-18 14:48:38 +1000

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="logo.png" className="App-logo img-fluid" alt="logo" />
+      </header>
+      Hello
+    </div>
+  );
+}
+
+export default App;
