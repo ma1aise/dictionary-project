8d127276784f0d509cea41715fb4dfd675f925f6
ma1aise <lisaewells03@outlook.com>
2024-03-24 17:30:25 +1000

    Made search input larger

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -26,7 +26,11 @@ export default function Dictionary() {
   return (
     <div className="Dictionary">
       <form onSubmit={search}>
-        <input type="search" onChange={handleKeywordChange} />
+        <input
+          className="search-input"
+          type="search"
+          onChange={handleKeywordChange}
+        />
       </form>
       <Results results={results} />
     </div>
