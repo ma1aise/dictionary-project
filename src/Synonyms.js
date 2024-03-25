a03ca3c87b2cf32bf2462db4b8e5491c1e74da51
ma1aise <lisaewells03@outlook.com>
2024-03-25 17:53:31 +1000

    Created Phonetics, Results and synonyms components

--- src/Synonyms.js
+++ src/Synonyms.js
@@ -2,6 +2,7 @@ import React from "react";
 
 export default function Synonyms(props) {
   if (props.synonyms) {
+    console.log(props.synonyms);
     return (
       <ul className="Synonyms">
         {props.synonyms.map(function (synonym, index) {
